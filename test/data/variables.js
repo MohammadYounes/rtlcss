a2902d29e67f6813fdd20d30078164a782495c1e
Iván Pereira Muñoz <elchininet@gmail.com>
2021-03-17 22:41:38 +0100

    Add aliases option to flip variables

--- test/data/variables.js
+++ test/data/variables.js
@@ -11,5 +11,26 @@ module.exports = [
     expected: ':root {--brightest: red}',
     input: ':root {--brightest: red}',
     reversable: true
+  },
+  {
+    should: 'Should flip variable values if it is flagged as an alias',
+    expected: ':root {--pad: 10px 4px 2px 5px}',
+    input: ':root {--pad: 10px 5px 2px 4px}',
+    reversable: true,
+    options: { aliases: { '--pad': 'padding' } }
+  },
+  {
+    should: 'Should not flip variable names starting with direction included in aliases',
+    expected: ':root {--left-margin: 10px}',
+    input: ':root {--left-margin: 10px}',
+    reversable: true,
+    options: { aliases: { '--left-margin': 'left' } }
+  },
+  {
+    should: 'Should not flip variable names containing direction included in aliases',
+    expected: ':root {--brightest: 10px}',
+    input: ':root {--brightest: 10px}',
+    reversable: true,
+    options: { aliases: { '--brightest': 'right' } }
   }
 ]
